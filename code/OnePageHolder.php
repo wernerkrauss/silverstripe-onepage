151378ad2fbb60ebc942a09c9672bcbad6356830
Werner Krauss <werner.krauss@netwerkstatt.at>
2014-09-11 16:58:06 +0200

    little cleanup

--- code/OnePageHolder.php
+++ code/OnePageHolder.php
@@ -2,23 +2,10 @@
 
 class OnePageHolder extends Page {
 
-	private static $db = array();
+    private static $singular_name = 'One Page Holder';
+    
+    private static $plural_name = 'One Page Holders';
 
-    private static $has_one = array();
-    
-    private static $has_many = array();
-    
-    private static $many_many = array();
-    
-    private static $belongs_many_many = array();
-    
-    private static $singular_name = '';
-    
-    private static $plural_name = 's';
-    
-    private static $summary_fields = array();
-    
-    private static $searchable_fields = array();
     
     public function getCMSFields() {
 		$fields = parent::getCMSFields();

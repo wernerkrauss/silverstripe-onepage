9df7eacb1bf7247f79606d2b42f5507c2ce07853
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:41:42 +1300

    Converted to PSR-2

--- code/OnePageHolder.php
+++ code/OnePageHolder.php
@@ -1,22 +1,23 @@
 <?php
 
-class OnePageHolder extends Page {
+class OnePageHolder extends Page
+{
 
     private static $singular_name = 'One Page Holder';
     
     private static $plural_name = 'One Page Holders';
 
-	private static $description = 'Holder for OnePage Slides. Shows all child pages as one big page';
+    private static $description = 'Holder for OnePage Slides. Shows all child pages as one big page';
 
     
-    public function getCMSFields() {
-		$fields = parent::getCMSFields();
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
 
-		return $fields;
-	}
+        return $fields;
+    }
 }
 
-class OnePageHolder_Controller extends Page_Controller {
-
-
+class OnePageHolder_Controller extends Page_Controller
+{
 }

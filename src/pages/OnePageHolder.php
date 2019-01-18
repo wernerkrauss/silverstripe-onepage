c4a7c282353712d4c42a998ff68a199491ba7281
Greg808 <32903200+Greg808@users.noreply.github.com>
2019-01-18 12:20:55 +0100

    Update OnePageHolder.php

--- src/pages/OnePageHolder.php
+++ src/pages/OnePageHolder.php
@@ -7,13 +7,13 @@ use Page;
 
 class OnePageHolder extends Page
 {
-
     private static $singular_name = 'One Page Holder';
 
     private static $plural_name = 'One Page Holders';
 
     private static $description = 'Holder for OnePage Slides. Shows all child pages as one big page';
 
+    private static $table_name = 'OnePageHolder';
 
     public function getCMSFields()
     {
